d28812e0d7ef1baa41c508a86aade63fd2678a99
marcg <marcgrec@tuta.com>
2026-05-29 13:39:38 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              rust-bin.stable.latest.default
+              lldb
+              ast-grep
+            ];
+          };
+      }
+    );
+}
