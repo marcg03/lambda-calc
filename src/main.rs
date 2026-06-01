1fb5a54b75137b22c2e193cfc88e9a1556035d22
marcg <marcgrec@tuta.com>
2026-06-01 09:51:37 +0300

    fix priting

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ impl fmt::Display for Expr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Expr::Var(name) => write!(f, "{}", name),
-            Expr::Lambda(param, body) => write!(f, "\\{} {}", param, body),
+            Expr::Lambda(param, body) => write!(f, "(\\{} {})", param, body),
             Expr::App(l, r) => write!(f, "({} {})", l, r),
         }
     }
